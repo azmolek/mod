cdc9a014468fa248e9c64c9c420822ef5ea15abe
Andy Zmolek <andy@zmolek.com>
2021-04-02 19:46:43 +0100

    Update index.js
    return some of the normal_x * normal_y

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ let fsSmooth = `
   void main (void) {
     float normal_x = (vpos.x + 1.0) * canvas_x / 2.0;
     float normal_y = (vpos.y + 1.0) * canvas_y / 2.0;
-    float color_value = mod(normal_x ** 2 + normal_y ** 2 + step, base);
+    float color_value = mod(normal_x * normal_y + step, base);
     float color_value_normal = color_value / (base - 1.0);
     color_value_normal = min(1.0 - color_value_normal, color_value_normal) * 2.0;
 
@@ -140,9 +140,9 @@ let fsColorBands = `
   void main (void) {
     float normal_x = (vpos.x + 1.0) * canvas_x / 2.0;
     float normal_y = (vpos.y + 1.0) * canvas_y / 2.0;
-    float color_value_r = mod(normal_x ** 2 + normal_y ** 2 + step, base);
-    float color_value_g = mod(normal_x ** 2 + normal_y ** 2 + step + (base / 3.0), base);
-    float color_value_b = mod(normal_x ** 2 + normal_y ** 2 + step + (2.0 * base / 3.0), base);
+    float color_value_r = mod(normal_x * normal_y + step, base);
+    float color_value_g = mod(normal_x * normal_y + step + (base / 3.0), base);
+    float color_value_b = mod(normal_x * normal_y + step + (2.0 * base / 3.0), base);
     float color_value_normal_r = color_value_r / (base - 1.0);
     float color_value_normal_g = color_value_g / (base - 1.0);
     float color_value_normal_b = color_value_b / (base - 1.0);
